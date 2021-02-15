649fd300bef75c4a3f1dba96c3ac9510e245aec5
Andrusarches <andrew.shornikov@gmail.com>
2021-02-16 00:53:04 +0300

    Начинаем программировать

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const MockMovieCardData = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  year: 2014
+};
+
+const {title, genre, year} = MockMovieCardData;
+
+ReactDOM.render(
+    <App title={title} genre={genre} year={year} />,
+    document.querySelector(`#root`)
+);
