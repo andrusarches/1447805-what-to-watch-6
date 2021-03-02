d01a5f4dad15bf640af701c4d5772a8b153e506f
Andrusarches <andrew.shornikov@gmail.com>
2021-03-03 01:52:49 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Main from '../main/main';
+import Login from '../login/login';
+import MyList from '../my-list/my-list';
+import Review from '../review/review';
+import Film from '../film/film';
+import Player from '../player/player';
+import NotFound from '../not-found/not-found';
 
 const App = (props) => {
   const {title, genre, year} = props;
 
   return (
-    <Main title={title} genre={genre} year={year}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={() => (
+          <Main title={title} genre={genre} year={year} />
+        )}
+        />
+        <Route path="/login" exact render={() => (
+          <Login />
+        )}
+        />
+        <Route path="/myList" exact render={() => (
+          <MyList />
+        )}
+        />
+        <Route path="/films/:id/review" exact render={() => (
+          <Review title={title} />
+        )}
+        />
+        <Route path="/films/:id" exact render={() => (
+          <Film title={title} genre={genre} year={year} />
+        )}
+        />
+        <Route path="/player/:id" exact render={() => (
+          <Player title={title} />
+        )}
+        />
+
+        <Route render={() => (
+          <NotFound />
+        )}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import UserBlock from '../user-block/user-block';
+
+const Header = () => {
+  return (
+    <header className="page-header movie-card__head">
+      <div className="logo">
+        <a href="main.html" className="logo__link">
+          <span className="logo__letter logo__letter--1">W</span>
+          <span className="logo__letter logo__letter--2">T</span>
+          <span className="logo__letter logo__letter--3">W</span>
+        </a>
+      </div>
+
+      <UserBlock />
+    </header>
+  );
+};
+
+export default Header;
