1867bf33acb96d9151761ffafda76acf16a32632
Andrusarches <andrew.shornikov@gmail.com>
2021-03-16 20:07:54 +0300

    module3-task1

--- src/components/add-review-form/add-review-form.jsx
+++ src/components/add-review-form/add-review-form.jsx
@@ -25,7 +25,7 @@ const AddReviewForm = () => {
       <form onSubmit={handleSubmit} className="add-review__form">
         <div className="rating">
           <div className="rating__stars">
-            <input className="rating__input" id="star-1" type="radio" name="rating" value="1" onChange={handleRatingChange}/>
+            <input className="rating__input" id="star-1" type="radio" name="rating" value="1" onChange={handleRatingChange} required/>
             <label className="rating__label" htmlFor="star-1">Rating 1</label>
 
             <input className="rating__input" id="star-2" type="radio" name="rating" value="2" onChange={handleRatingChange}/>
@@ -58,7 +58,7 @@ const AddReviewForm = () => {
         </div>
 
         <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" onChange={handleTextChange}></textarea>
+          <textarea minLength="50" maxLength="400" className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" onChange={handleTextChange}></textarea>
           <div className="add-review__submit">
             <button className="add-review__btn" type="submit">Post</button>
           </div>
